698d165699a102ba4f53297f4924ac982f38808b
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-09-19 20:32:36 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Maximum triplet sum in array/maximum-triplet-sum-in-array.java
@@ -0,0 +1,78 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+class Solution{
+    
+  
+    int maxTripletSum(int a[], int n)
+    {
+        // Complete the function
+        Arrays.sort(a);
+        int sum=0;
+        int end=n-1;
+        int count=0;
+        
+        while(end>=0){
+            sum=sum+a[end];
+            end--;
+            count++;
+            
+            if(count==3){
+                break;
+            }
+        }
+        return sum;
+    }
+  
+    
+}
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+    
+    // Driver code
+	public static void main (String[] args) throws IOException{
+		// Taking input using buffered reader
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int testcases = Integer.parseInt(br.readLine());
+		
+		// looping through all testcases
+		while(testcases-- > 0){
+		    
+		    String line1 = br.readLine();
+		    String[] element = line1.trim().split("\\s+");
+		    int sizeOfArray = Integer.parseInt(element[0]);
+		    
+		    int arr [] = new int[sizeOfArray];
+		    
+		    String line = br.readLine();
+		    String[] elements = line.trim().split("\\s+");
+		    
+		    for(int i = 0;i<sizeOfArray;i++){
+		        arr[i] = Integer.parseInt(elements[i]);
+		    }
+		    
+		    Solution obj = new Solution();
+		    int res = obj.maxTripletSum(arr, sizeOfArray);
+		    System.out.println(res);
+		}
+	}
+}
+
+
+// } Driver Code Ends
