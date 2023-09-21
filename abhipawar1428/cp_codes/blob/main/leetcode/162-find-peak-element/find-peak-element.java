7116d10e446885a1a0e2c91f6deada468b9b1cf2
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-09-21 23:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (96.94%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/162-find-peak-element/find-peak-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }else if(nums[1]<nums[0]){
+            return 0;
+        }else if(nums[nums.length-1] > nums[nums.length-2]){
+            return nums.length-1;
+        }
+        for(int i=1;i<nums.length-1;i++){
+            if(nums[i-1]<nums[i] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
