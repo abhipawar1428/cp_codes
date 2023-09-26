456c0e5c5b9854f41d5335b15a5efdce45b17c53
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-09-26 07:14:54 +0530

    Time: 6 ms (13.74%) | Memory: 44.3 MB (92.24%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,20 +1,31 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int arr[]=new int[nums1.length+nums2.length];
+        //int arr[]=new int[nums1.length+nums2.length];
+        ArrayList<Integer> arr=new ArrayList<>();
+        
 
         for(int i=0;i<nums1.length;i++){
-            arr[i]=nums1[i];
+            //arr[i]=nums1[i];
+            arr.add(nums1[i]);
         }
         for(int i=0;i<nums2.length;i++){
-            arr[nums1.length+i]=nums2[i];
+            //arr[nums1.length+i]=nums2[i];
+            arr.add(nums2[i]);
         }
-        Arrays.sort(arr);
+        //Arrays.sort(arr);
+        Collections.sort(arr);
 
-        if(arr.length%2!=0){
-            return arr[arr.length/2];
+        //if(arr.length%2!=0){
+        if(arr.size()%2!=0){
+            //return arr[arr.length/2];
+            return arr.get(arr.size()/2);
         }else{
-            double ret=arr[arr.length/2]+arr[(arr.length-1)/2];
-            return ret/2;
+            //double ret=arr[arr.length/2]+arr[(arr.length-1)/2];
+            //return ret/2;
+            double ptr1=arr.get(arr.size()/2);
+            double ptr2=arr.get((arr.size()-1)/2);
+
+            return (ptr1+ptr2)/2;
         }
 
     }
