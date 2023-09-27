bbf667f9ca46cfc77e0525e2c75bd2f73b57d257
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-09-27 15:32:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Least Prime Factor/least-prime-factor.java
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            Solution ob = new Solution();
+            int[] ans = ob.leastPrimeFactor(n);
+            for(int i = 1; i <=n; i++)
+            {
+                out.print(ans[i] + " ");
+            }
+            out.println();
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public int[] leastPrimeFactor(int n)
+    {
+        // code here
+        int arr[]=new int[n+1];
+        if(n>1){
+            arr[0]=0;
+            arr[1]=1;
+        }
+        for(int i=2;i<=n;i++){
+            while(i>1){
+                for(int j=2;j<=i;j++){
+                    if(i%j==0){
+                        arr[i]=j;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        return arr;
+    }
+}
