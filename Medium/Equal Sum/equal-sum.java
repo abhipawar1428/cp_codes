e07e77652bbe4771df4dacd979509501fbd671f0
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-10-05 00:55:26 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Equal Sum/equal-sum.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(str[i]);
+            }
+
+            String ans = new Solution().equilibrium(a, n);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    String equilibrium(int arr[], int n) {
+        // code here
+        for(int i=1;i<arr.length;i++){
+            arr[i]=arr[i]+arr[i-1];
+            //System.out.println(nums[i]);
+        }
+        for(int i=0;i<n;i++){
+            int leftsum=0;
+            int rightsum=0;
+            if(i==0){
+                leftsum=0;
+            }else{
+                leftsum=arr[i-1];
+            }
+
+            if(i==n-1){
+                rightsum=0;
+            }else{
+                rightsum=arr[n-1]-arr[i];
+            }
+
+            if(leftsum==rightsum){
+                return "YES";
+            }
+        }    
+        return "NO";
+    }
+}
