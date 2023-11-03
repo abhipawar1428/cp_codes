1ff8552c56a75c1a11ca7ce9b7ff5d3e99e18adf
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-11-03 17:30:17 +0530

    Time: 3 ms (96.64%) | Memory: 43.6 MB (64.06%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/2886-faulty-keyboard/faulty-keyboard.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String finalString(String s) {
+        StringBuffer sb=new StringBuffer();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='i'){
+                sb.append(s.charAt(i));
+            }else{
+                sb.reverse();
+            }
+        }
+        return sb.toString();
+    }
+}
