b9a805c1a0595cc8e10020eb6ad9909325db3f62
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-11-04 14:03:20 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (24.63%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        else if(n==2){
+            return 2;
+        }else{
+            int a=1;
+            int b=2;
+            int c=a+b;
+
+            for(int i=3;i<=n;i++){
+                c=a+b;
+                a=b;
+                b=c;
+            }
+            return c;
+        }
+ 
+
+
+    }
+}
