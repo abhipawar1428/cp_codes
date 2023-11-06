4c8ce773b279f5bd1a58d81bb91a193f6c944981
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-11-06 21:21:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (72.71%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/268-missing-number/missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int s=(n*(n+1))/2;
+
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+
+        return s-sum;
+    }
+}
