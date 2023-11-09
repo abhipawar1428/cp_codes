63c496890e9b8d9a7d3819e6cf695cd5df7b8ec8
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-11-09 17:25:07 +0530

    Time: 0 ms (100.00%) | Memory: 39.1 MB (43.97%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -9,11 +9,10 @@ class Solution {
         // return c;
         if(high%2!=0){
             return (high-low)/2+1;
-        }else{
-            if(low%2!=0)
+        }else if(low%2!=0){
                 return (high-low)/2+1;
-            else
+        }else
                 return (high-low)/2;
-        }
+        
     }
 }
