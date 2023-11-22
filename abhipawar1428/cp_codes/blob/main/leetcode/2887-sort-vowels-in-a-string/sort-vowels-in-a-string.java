56a903244a07409c373cf921c55da07a28440022
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-11-22 19:21:52 +0530

    Time: 59 ms (38.31%) | Memory: 47.3 MB (27.65%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String sortVowels(String s) {
+        ArrayList<Character> al=new ArrayList<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='A' ||
+               s.charAt(i)=='e' || s.charAt(i)=='E' ||
+               s.charAt(i)=='i' || s.charAt(i)=='I' ||
+               s.charAt(i)=='o' || s.charAt(i)=='O' ||
+               s.charAt(i)=='u' || s.charAt(i)=='U' ){
+                   al.add(s.charAt(i));
+               }
+        }
+        Collections.sort(al);
+        StringBuffer sb=new StringBuffer("");
+        int j=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a' || s.charAt(i)=='A' ||
+               s.charAt(i)=='e' || s.charAt(i)=='E' ||
+               s.charAt(i)=='i' || s.charAt(i)=='I' ||
+               s.charAt(i)=='o' || s.charAt(i)=='O' ||
+               s.charAt(i)=='u' || s.charAt(i)=='U' ){
+                   sb.append(al.get(j));
+                   j++;
+               }else{
+                   sb.append(s.charAt(i));
+               }
+        }
+        return sb.toString();
+    }
+}
