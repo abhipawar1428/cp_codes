c6c789a23e36768f07997021a9f6e8d61bd173b5
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2023-12-26 23:24:24 +0530

    Time: 1 ms (47.89%) | Memory: 40.5 MB (5.48%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -1,9 +1,14 @@
 class Solution {
     public int kthFactor(int n, int k) {
         ArrayList<Integer> al=new ArrayList<>();
+        int count=0;
         for(int i=1;i<=n/2;i++){
             if(n%i==0){
                 al.add(i);
+                count++;
+            }
+            if(count==k){
+                return al.get(k-1);
             }
         }
         al.add(n);
