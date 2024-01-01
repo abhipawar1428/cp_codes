3cc1ed1f49dd44ba8c2e698fed774d92e77d1f87
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-01 23:14:21 +0530

    Time: 1 ms (94.95%) | Memory: 44.9 MB (8.69%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,8 +3,7 @@ class Solution {
         int count=0;
         for(int i=0;i<nums.length-1;i++){
             if(nums[i]!=nums[i+1]){
-                count++;
-                nums[count]=nums[i+1];
+                nums[++count]=nums[i+1];
             }
         }
         return count+1;
