f3aec7a27311b71d36036aea7dd86fd08b5ef5f9
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-04 18:18:21 +0530

    Time: 2 ms (79.40%) | Memory: 41.6 MB (11.10%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()%2==1){
+            return false;
+        }else if(s.length()==0){
+            return true;
+        }else{
+            Stack<Character> stack=new Stack<>();
+            for(int i=0;i<s.length();i++){
+                char ch=s.charAt(i);
+                if(ch=='(' || ch=='[' || ch=='{'){
+                    stack.push(ch);
+                }else{
+                    if(stack.empty()){
+                       return false; 
+                    }else{
+                        char ch2=stack.peek();
+                        if((ch==')' && ch2=='(') || (ch==']' && ch2=='[') || (ch=='}' && ch2=='{')){
+                            stack.pop();
+                        }else{
+                            System.out.print("...");
+                            return false;
+                        }
+                    }
+                }
+            }
+            if(stack.empty())
+                return true;
+            else
+                return false;
+        }
+    }
+}
