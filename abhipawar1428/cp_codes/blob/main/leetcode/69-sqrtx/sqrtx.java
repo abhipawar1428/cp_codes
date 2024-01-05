9c5b7f6e758fcf621520c08458405e98c47b7472
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-05 19:43:59 +0530

    Time: 1 ms (96.53%) | Memory: 40.7 MB (17.55%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/69-sqrtx/sqrtx.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/69-sqrtx/sqrtx.java
@@ -1,17 +1,23 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0)
+        if(x==0){
             return 0;
-        else{
-            long i=1;
-            while(i<=x){
-                if(i*i>x){
-                    return (int)i-1;
-                }else
-                    i++;
+        }else{
+            long l=1;
+            long r=x;
 
+            long mid=-1;
+            while(l<=r){
+                mid=(l+r)/2;
+                if(mid*mid==x){
+                    return (int)mid;
+                }else if(mid*mid>x){
+                    r=mid-1;
+                }else{
+                    l=mid+1;
+                }
             }
-            return 1;
-        } 
+            return (int)r;
+        }
     }
 }
