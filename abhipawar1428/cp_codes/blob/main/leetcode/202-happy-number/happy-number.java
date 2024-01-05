53bc7485a7679567aa9257993331c697f36223fc
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-05 20:29:35 +0530

    Time: 1 ms (82.08%) | Memory: 40.4 MB (26.84%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/202-happy-number/happy-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int sum=0;
+        HashSet<Integer> hs=new HashSet<>();
+        while(n!=0){
+            int r=n%10;
+            sum=sum+(r*r);
+            n=n/10;
+
+            if(n==0){
+                if(sum==1){
+                    return true;
+                }else{
+                    n=sum;
+                    sum=0;
+                    if(!hs.add(n)){
+                        return false;
+                    }else{
+                        hs.add(n);
+                    }
+
+                }
+            }
+        }
+        return false;
+    }
+}
