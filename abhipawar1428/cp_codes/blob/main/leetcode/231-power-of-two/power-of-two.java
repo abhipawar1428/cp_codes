95ed1fc93a7a12b23244afbe7773e01bb55335f7
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-06 12:12:38 +0530

    Time: 1 ms (81.34%) | Memory: 40.9 MB (12.79%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/231-power-of-two/power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        int i=0;
+        while(Math.pow(2,i)<=n){
+            if(Math.pow(2,i)==n){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+}
