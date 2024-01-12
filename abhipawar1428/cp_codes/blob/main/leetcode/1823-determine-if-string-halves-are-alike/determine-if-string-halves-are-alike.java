73d0468e589d42591795a78c2c039217111a0224
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-12 13:36:26 +0530

    Time: 7 ms (33.95%) | Memory: 42.1 MB (17.05%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
         s=s.toLowerCase();
-        StringBuffer sb=new StringBuffer(s.substring(s.length()/2,s.length()));
         int s1=0;
-        int s2=0;
-
-        for(int i=0;i<sb.length();i++){
+        for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
-            char ch2=sb.charAt(i);
-            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-                s1++;
+            if(i<s.length()/2){
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                    s1++;
+                }
+            }else{
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                    s1--;
+                }
             }
-            if(ch2=='a' || ch2=='e' || ch2=='i' || ch2=='o' || ch2=='u'){
-                s2++;
-            }
+
         }
-        if(s1==s2){
+        if(s1==0)   
             return true;
-        }else{
+        else
             return false;
-        }
     }
 }
