d73e78c10ed2a420cb6611d768d62a32ef3b1b63
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-14 13:21:53 +0530

    Time: 78 ms (24.82%) | Memory: 45.5 MB (27.19%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -3,31 +3,27 @@ class Solution {
         if(word1.length()!=word2.length()){
             return false;
         }else{
-            TreeMap<Character,Integer> map=new TreeMap<>();
+            HashMap<Character,Integer> map=new HashMap<>();
             for(int i=0;i<word1.length();i++){
                 map.put(word1.charAt(i),map.getOrDefault(word1.charAt(i),0)+1);
             }
-            TreeMap<Character,Integer> map2=new TreeMap<>();
+            HashMap<Character,Integer> map2=new HashMap<>();
             for(int i=0;i<word1.length();i++){
                 map2.put(word2.charAt(i),map2.getOrDefault(word2.charAt(i),0)+1);
             }
             if(map.size()==map2.size()){
-                for(char key:map.keySet()){
-                    if(!map2.containsKey(key)){
-                        return false;
-                    }
-                }
-
                 int a1[]=new int[map.size()];
                 int a2[]=new int[map.size()];
 
                 int i=0;
-                for(int value:map.values()){
-                    a1[i++]=value;
-                }
-                i=0;
-                for(int value:map2.values()){
-                    a2[i++]=value;
+                for(char key:map.keySet()){
+                    if(!map2.containsKey(key)){
+                        return false;
+                    }else{
+                        a1[i]=map.get(key);
+                        a2[i]=map2.get(key);
+                        i++;
+                    }
                 }
                 Arrays.sort(a1);
                 Arrays.sort(a2);
