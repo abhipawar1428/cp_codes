0eb6fb60838945f07a709459a5b5434ff2d0f17d
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-17 18:46:40 +0530

    Time: 2 ms (98.10%) | Memory: 42.1 MB (5.39%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/1319-unique-number-of-occurrences/unique-number-of-occurrences.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for(int value:map.values()){
+            if(!set.add(value)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
