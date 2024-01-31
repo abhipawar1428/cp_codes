411cc5ef9b342df0ea6a46051bff932808520b10
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-01-31 19:05:30 +0530

    Time: 1 ms (100.00%) | Memory: 41.6 MB (92.34%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.java
@@ -3,7 +3,6 @@ class Solution {
         StringBuilder sb=new StringBuilder(s);
         while(sb.indexOf(part)!=-1){
             sb.delete(sb.indexOf(part),sb.indexOf(part)+part.length());
-            System.out.println(sb);
         }
         return sb.toString();
         
