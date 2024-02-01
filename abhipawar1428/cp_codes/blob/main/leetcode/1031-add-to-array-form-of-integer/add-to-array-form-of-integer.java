c1cfec7660c849e349c52b7b3ed7dfbe4f44552f
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-02-01 21:03:46 +0530

    Time: 56 ms (17.15%) | Memory: 45.5 MB (68.08%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/1031-add-to-array-form-of-integer/add-to-array-form-of-integer.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> addToArrayForm(int[] num, int k) {
+        String num2=Integer.toString(k);
+        int ptr1=num.length-1;
+        int ptr2=num2.length()-1;
+
+        List<Integer> ret=new ArrayList<>();
+        int carry=0;
+        while(ptr1!=-1 && ptr2!=-1){
+            int sum=num[ptr1]+(num2.charAt(ptr2)-'0') + carry;
+
+            if(sum<10){
+                carry=0;
+                ret.addFirst(sum);
+            }else{
+                carry=1;
+                ret.addFirst(sum%10);
+            }
+            ptr1--;
+            ptr2--;
+        }
+        while(ptr1!=-1){
+            int sum=num[ptr1--]+carry;
+            if(sum<10){
+                carry=0;
+                ret.addFirst(sum);
+            }else{
+                carry=1;
+                ret.addFirst(sum%10);
+            }
+        }
+        while(ptr2!=-1){
+            int sum=num2.charAt(ptr2--)-'0'+carry;
+            if(sum<10){
+                carry=0;
+                ret.addFirst(sum);
+            }else{
+                carry=1;
+                ret.addFirst(sum%10);
+            }
+        }
+        if(carry==1){
+            ret.addFirst(1);
+        }
+        return ret;
+    }
+}
