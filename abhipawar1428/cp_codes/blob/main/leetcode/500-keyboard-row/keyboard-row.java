1485bb8a53d3c49e6dc0cb5f5df474073c67033d
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-02-05 00:05:05 +0530

    Time: 1 ms (55.25%) | Memory: 41.1 MB (69.71%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/500-keyboard-row/keyboard-row.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        StringBuffer sb=new StringBuffer("qwertyuiop");
+        StringBuffer sb2=new StringBuffer("asdfghjkl");
+        StringBuffer sb3=new StringBuffer("zxcvbnm");
+        ArrayList<String> ret=new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            boolean result=false;
+            String string=words[i].toLowerCase();
+            if(sb.indexOf(Character.toString(string.charAt(0))) != -1){
+                result=isInRow(sb,words[i].toLowerCase());
+            }else if(sb2.indexOf(Character.toString(string.charAt(0))) != -1){
+                result=isInRow(sb2,words[i].toLowerCase());
+            }else{
+                result=isInRow(sb3,words[i].toLowerCase());
+            }
+            if(result){
+                ret.add(words[i]);
+            }
+        }
+        String retArr[]=new String[ret.size()];
+        for(int i=0;i<retArr.length;i++){
+            retArr[i]=ret.get(i);
+        }
+        return retArr;
+    }
+    boolean isInRow(StringBuffer sb,String str){
+        for(int i=1;i<str.length();i++){
+            if(sb.indexOf(Character.toString(str.charAt(i)))==-1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
