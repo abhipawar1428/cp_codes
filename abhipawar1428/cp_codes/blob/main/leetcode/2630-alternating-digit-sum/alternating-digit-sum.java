1faa9f70aa2cda34052f7a11418304490482714c
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-02-05 00:20:57 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (16.19%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/2630-alternating-digit-sum/alternating-digit-sum.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/2630-alternating-digit-sum/alternating-digit-sum.java
@@ -1,26 +1,13 @@
 class Solution {
     public int alternateDigitSum(int n) {
         int sum=0;
-        int count=0;
-        int temp=n;
-        while(n!=0){
-            count++;
-            n/=10;
+        String num=Integer.toString(n);
+        for(int i=0;i<num.length();i+=2){
+            sum+=num.charAt(i)-'0';
         }
-        n=temp;
-            while(n!=0){
-                if(count%2==0){
-                    sum-=n%10;
-                    n/=10;
-                    count--;
-                }else{
-                    sum+=n%10;
-                    n/=10;
-                    count--;
-                }
-            }
-            return sum;
-        
-        
+        for(int i=1;i<num.length();i+=2){
+            sum-=num.charAt(i)-'0';
+        }
+        return sum;
     }
 }
