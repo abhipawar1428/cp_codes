0644f7617a31a08595d9de74dbca9e6b0652e6c0
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-02-14 12:28:23 +0530

    Time: 3 ms (100.00%) | Memory: 76.7 MB (80.13%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Queue<Integer> negative=new LinkedList<>();
-        Queue<Integer> positive=new LinkedList<>();
-
+        int[] ans=new int[nums.length];
+        int pos=0;
+        int neg=1;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<0)
-                negative.offer(nums[i]);
-            else
-                positive.offer(nums[i]);
+            if(nums[i]<0){
+                ans[neg]=nums[i];
+                neg+=2;
+            }else{
+                ans[pos]=nums[i];
+                pos+=2;
+            }
         }
-        for(int i=0;i<nums.length;i++){
-            nums[i++]=positive.poll();
-            nums[i]=negative.poll();
-        }
-        return nums;
+        return ans;
     }
 }
