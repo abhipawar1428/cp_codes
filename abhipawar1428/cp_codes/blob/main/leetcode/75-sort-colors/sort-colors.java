f6803655920f6ce40c3e28fb51a4ca63d1358d16
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-06-11 16:50:52 +0530

    Time: 3 ms (6.82%) | Memory: 41.8 MB (27.03%) - LeetSync

---
+++ abhipawar1428/cp_codes/blob/main/leetcode/75-sort-colors/sort-colors.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int z=0,o=0,t=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==0){
+                z++;
+            }else if(nums[i]==1){
+                o++;
+            }else{
+                t++;
+            }
+        }
+        System.out.println(z);
+                System.out.println(o);
+
+        System.out.println(t);
+
+        for(int i=0;i<nums.length;i++){
+            if(i<z){
+                nums[i]=0;
+            }else if(i<(o+z)){
+                nums[i]=1;
+            }else{
+                nums[i]=2;
+            }
+        }
+
+    }
+}
