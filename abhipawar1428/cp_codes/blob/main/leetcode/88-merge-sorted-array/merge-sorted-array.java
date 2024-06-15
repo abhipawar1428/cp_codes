dea5aa70c9d1a60c5808fb1c6129c783b56bdd07
Abhishek Pawar <119572674+abhipawar1428@users.noreply.github.com>
2024-06-15 11:21:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (47.30%) - LeetSync

--- abhipawar1428/cp_codes/blob/main/leetcode/88-merge-sorted-array/merge-sorted-array.java
+++ abhipawar1428/cp_codes/blob/main/leetcode/88-merge-sorted-array/merge-sorted-array.java
@@ -1,17 +1,19 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] arr=new int[m+n];
-
-        int i=0,j=0,k=0;
+        int arr[]=new int[m+n];
+        
+        int i=0,j=0;
+        int k=0;
         while(i<m && j<n){
             if(nums1[i]<nums2[j]){
                 arr[k]=nums1[i];
                 i++;
+                k++;
             }else{
                 arr[k]=nums2[j];
                 j++;
+                k++;
             }
-            k++;
         }
         while(i<m){
             arr[k]=nums1[i];
@@ -23,9 +25,9 @@ class Solution {
             j++;
             k++;
         }
-
-        for(i=0;i<arr.length;i++){
-            nums1[i]=arr[i];
+        for(int l=0;l<arr.length;l++){
+            nums1[l]=arr[l];
         }
+        
     }
 }
